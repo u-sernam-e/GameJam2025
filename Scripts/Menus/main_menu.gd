32c813aa4976e1cb92c509f985f82f7869d2220a
Billy Harris <billythecorndog@gmail.com>
2025-11-08 17:15:51 -0500

    transition
    transition beginnings

--- Scripts/Menus/main_menu.gd
+++ Scripts/Menus/main_menu.gd
@@ -1,4 +1,5 @@
 extends Control
-
+@onready var animation_player = $TransitionController/ColorRect/AnimationPlayer
 func _on_button_pressed() -> void:
+	animation_player.play("fadeout")
 	get_tree().change_scene_to_file("res://Scenes/Level/main_level.tscn")
