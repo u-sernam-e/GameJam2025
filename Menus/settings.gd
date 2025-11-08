b1fc85f40905681de7b959daf8b1f83de464c37c
lcarstens-bing <carstens.jack.liam@gmail.com>
2025-11-07 19:42:14 -0500

    added menus

---
+++ Menus/settings.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+func _on_h_slider_value_changed(value: float) -> void:
+	pass # Replace with function body.
+
+
+func _on_button_pressed() -> void:
+	pass # Replace with function body.
